1d955f529bc851b42088089a27eecc7a85d013fc
Mikael Håkansson <wmmihaa@hotmail.com>
2015-10-26 09:30:47 +0100

    Added

---
+++ simulator.js
@@ -0,0 +1,23 @@
+﻿require('colors');
+var MicroServiceBusHost = require("./microServiceBusHost.js");
+var fs = require('fs');
+
+var settigs = getSettings();
+
+settigs.hostName = "nodeJs-00005";
+new MicroServiceBusHost(settigs).Start();
+settigs.hostName = "nodeJs-00004";
+new MicroServiceBusHost(settigs).Start();
+
+
+function getSettings() {
+    try {
+        var data = fs.readFileSync('./settings.json');
+        var settings = JSON.parse(data);
+        return settings;
+    }
+    catch (err) {
+        console.log('Invalid settings file.'.red);
+        process.abort();
+    }
+}
