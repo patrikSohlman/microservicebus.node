807201d9934915bde5b77832750a97bea23411f8
Mikael Håkansson <wmmihaa@hotmail.com>
2015-11-09 13:40:22 +0100

    ..

--- Tests/UnitTests.js
+++ Tests/UnitTests.js
@@ -3,7 +3,7 @@ var util = require('../Utils.js');
 var test = require('unit.js');
 var fs = require('fs');
 var assert = require('assert');
-var request = require('sync-request');
+var request = require('supertest');
 var MicroServiceBusHost = require("../microServiceBusHost.js");
 
 var settings;
@@ -16,11 +16,11 @@ describe('SignIn', function () {
         if (orgId == undefined)
             throw "organizationId is not set as an environment variable";
     });
-     
+
     it('Save settings should work', function () {
         
         settings = {
-            "debug": true,
+            "debug": false,
             "hubUri": "wss://microservicebus.com",
             "nodeName": "TestNode1",
             "organizationId" : process.env.organizationid,
@@ -37,13 +37,30 @@ describe('SignIn', function () {
                     it('Should start without errors', function () {
                         exceptionCount.should.equal(0);
                     });
-                    it('microservice.js should exist after login', function () {
+                    it('simulatorTemperatureSensor.js should exist after login', function () {
                         fs.statSync(__dirname + "/../Services/simulatorTemperatureSensor.js");
                     });
-                    it('microservice.js should exist after login', function () {
+                    it('nullOutboundService.js should exist after login', function () {
                         fs.statSync(__dirname + "/../Services/nullOutboundService.js");
                     });
-                    
+                    it('calling test should work', function () {
+                        var url = 'http://localhost:9090';
+                        request(url)
+		                    .get('/api/data/azureApiAppInboundService1/test')
+		                    .send('')
+		                    .expect('Content-Type', /json/)
+		                    .expect(200)//Status code
+		                    .end(function (err, res) {
+                                if (err) {
+                                    throw err;
+                                }
+                            
+                                console.log('HELLO');
+                                res.body.should.have.property('result');
+                                res.body.result.should.equal(false);
+                                done();
+                            });
+                    });
                 });
             });
             loggedInComplete = true;
@@ -58,9 +75,29 @@ describe('SignIn', function () {
                 console.log();
             }
         }
+        
+     
     });
 });
 
 
-
-
+//describe('Call service', function () {
+//    it('calling test should work', function () {
+//        var url = 'http://localhost:9090';
+//        request(url)
+//		.get('/api/data/azureApiAppInboundService1/test')
+//		.send('')
+//		.expect('Content-Type', /json/)
+//		.expect(200)//Status code
+//		.end(function (err, res) {
+//            if (err) {
+//                throw err;
+//            }
+            
+//            console.log('HELLO');
+//            res.body.should.have.property('result');
+//            res.body.result.should.equal(false);
+//            done();
+//        });
+//    });
+//});
