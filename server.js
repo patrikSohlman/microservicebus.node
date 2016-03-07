86b0be4fa60e1789cc53c19424d554673093573e
Mikael Håkansson <wmmihaa@hotmail.com>
2016-03-07 19:55:52 +0100

    ...

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
 ﻿/*
 The MIT License (MIT)
-
 Copyright (c) 2014 microServiceBus.com
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,19 +20,18 @@ SOFTWARE.
 
 var fs = require('fs');
 
-
 try {
+    var port = process.env.PORT || 1337;
     var settings = {
-        "debug": process.env['debug'] == "true",
-        "hubUri": process.env['hubUri'],
-        "port": parseInt( process.env['port']),
-        "trackMemoryUsage": process.env['trackMemoryUsage'],
+        "debug": "true",
+        "hubUri": process.env['hubUri'] ,
+        "port": port,
+        "trackMemoryUsage": 0,
         "nodeName": process.env['nodeName'],
-        "organizationId": process.env['organizationId'],
-        "machineName": process.env['machineName']
+        "organizationId": process.env['organizationId']
     }
-    fs.writeFileSync('./settings.json', data);
-    require("./start.js");
+    fs.writeFileSync('./settings.json', JSON.stringify(settings));
+    var hub = require("./start.js");
 
 }
 catch (err) {
